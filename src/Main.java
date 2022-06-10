ad440e80cbdfbb102c0df3350d1928a1cf0e13ad
Blazej Koziel <s21116@pjwstk.edu.pl>
2022-06-10 05:04:19 +0200

    a small fix

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
 import java.util.*;
 
-/*
--------------------------------------
-   -- author
-   s21116
-   grupa: 14c
-   Imie i nazwisko: Błażej Kozieł
--------------------------------------
- */
-
 public class Main {
     public static void main(String[] args) {
 
@@ -19,23 +10,10 @@ public class Main {
             Data testData = new Data(test);
             List<List<Double>> t1 = testData.getX();
             String result; int correctAnswers = 0, allAnswers = t1.size();
-            System.out.println("Enter \"k\": ");
+            System.out.println("Enter \"k\" nearest neighbors: ");
             Scanner in = new Scanner(System.in);
             int k = in.nextInt();
-/*
-            for (int i=1; i<=120; i++){
-                correctAnswers=0;
-                for (List<Double> doubles : t1) {
-                    result = Classification.classify(Normalization.normalize(trainingData.getX(), doubles), i);
-                    if (result.length()==doubles.get(doubles.size()-1)){
-                        correctAnswers++;
-                    }
-                }
-                //System.out.println("accuracy of the experiment: "+100*(double) correctAnswers/allAnswers+"");
-                System.out.println(correctAnswers);
-            }
 
- */
             for (List<Double> doubles : t1) {
                 result = Classification.classify(Normalization.normalize(trainingData.getX(), doubles), k);
                 if (result.length()==doubles.get(doubles.size()-1)){
@@ -49,12 +27,14 @@ public class Main {
 
             int number = trainingData.getX().get(0).size()-1;
             for (;;){
-                System.out.println("\nchoose: \n- 1 to enter a vector\n- 2 to leave ");
+                System.out.println("\nchoose: \n- 1 to enter a vector (new flower)" +
+                        "\n- 2 to leave ");
                 int input = in.nextInt();
                 if (input==2){
                     break;
                 }else if (input==1) {
-                    System.out.println("type " + number + " numbers and confirm each of them with enter ");
+                    System.out.println("type " + number + " numbers and confirm each of them by enter" +
+                            " (4 values represents 4 attributes  of flower.)");
                     List<Double> array = new ArrayList<>();
                     String q;
                     for (int c = 0; c < number; c++) {
@@ -62,12 +42,12 @@ public class Main {
                         q = q.replaceAll(",", ".");
                         array.add(Double.parseDouble(q));
                     }
-                    System.out.print("Now, tpye \"k\":  ");
+                    System.out.print("Now, type \"k\" nearest neighbors:  ");
                     k = in.nextInt();
                     q = Classification.classify(Normalization.normalize(trainingData.getX(),array), k);
                     System.out.println("Answer: "+q);
                 } else {
-                    System.out.println("Incvalid value, please try again");
+                    System.out.println("Invalid value, please try again");
                 }
             }
 
